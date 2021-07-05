81265ac38c519e083d94e3182bbd012ff9915500
vasiltatarov <vasiltatarov3@gmail.com>
2021-07-05 15:57:42 +0300

    add delete operation

---
+++ WebApi.Data/Migrations/20210705110223_Initialize.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApi.Data.Migrations
+{
+    public partial class Initialize : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    IMDB = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
---
+++ WebApi/Controllers/BookController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Data;
+using WebApi.Data.Models;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookController : ControllerBase
+    {
+        private readonly ApplicationDbContext data;
+
+        public BookController(ApplicationDbContext data) => this.data = data;
+
+        [HttpGet]
+        public IEnumerable<Book> Get()
+            => this.data.Books.ToArray();
+
+        [HttpPost]
+        public async Task<JsonResult> Post(Book book)
+        {
+            if (!ModelState.IsValid || book.Year <= 0 || book.IMDB <= 0)
+            {
+                return new JsonResult("Model is invalid. The fields are required!");
+            }
+
+            await this.data.Books.AddAsync(new Book
+            {
+                Name = book.Name,
+                Author = book.Author,
+                Year = book.Year,
+                IMDB = book.IMDB,
+            });
+            await this.data.SaveChangesAsync();
+
+            return new JsonResult("Added Successfully");
+        }
+
+        [HttpPut]
+        public async Task<JsonResult> Put(Book book)
+        {
+            if (!ModelState.IsValid || book.Year <= 0 || book.IMDB <= 0)
+            {
+                return new JsonResult("Model is invalid. The fields are required!");
+            }
+
+            var entity = await this.data.Books.FindAsync(book.Id);
+            if (entity == null)
+            {
+                return new JsonResult("Book not exist!");
+            }
+
+            entity.Name = book.Name;
+            entity.Author = book.Author;
+            entity.Year = book.Year;
+            entity.IMDB = book.IMDB;
+
+            await this.data.SaveChangesAsync();
+
+            return new JsonResult("Book updated successfully.");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<JsonResult> Delete(int id)
+        {
+            var book = await this.data.Books.FindAsync(id);
+            if (book == null)
+            {
+                return new JsonResult("Book not exist!");
+            }
+
+            this.data.Books.Remove(book);
+            await this.data.SaveChangesAsync();
+
+            return new JsonResult("Book deleted successfully.");
+        }
+    }
+}
